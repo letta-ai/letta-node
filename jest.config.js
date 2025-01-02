e16ae8b4533c37cd3b6d3cad14ccdfa963908105
cthomas <caren@letta.com>
2025-01-02 14:50:01 -0800

    Add custom unit tests (#10)
    * add unit tests
    * propagate api key

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
+    moduleNameMapper: {
+        "^environments$": "<rootDir>/src/environments.ts",
+        "^api$": "<rootDir>/src/api/index.ts",
+        "^api/(.*)$": "<rootDir>/src/api/$1"
+    },
+    modulePaths: [
+        "<rootDir>/src"
+    ]
 };
