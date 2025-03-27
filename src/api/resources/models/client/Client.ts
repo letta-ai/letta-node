9271d23ffcd5e5cb85a4994fb275980e3b5369a5
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-03-27 00:10:56 +0000

    Release 0.1.76

--- src/api/resources/models/client/Client.ts
+++ src/api/resources/models/client/Client.ts
@@ -51,8 +51,8 @@ export class Models {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@letta-ai/letta-client",
-                "X-Fern-SDK-Version": "0.1.75",
-                "User-Agent": "@letta-ai/letta-client/0.1.75",
+                "X-Fern-SDK-Version": "0.1.76",
+                "User-Agent": "@letta-ai/letta-client/0.1.76",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
@@ -114,8 +114,8 @@ export class Models {
             headers: {
                 "X-Fern-Language": "JavaScript",
                 "X-Fern-SDK-Name": "@letta-ai/letta-client",
-                "X-Fern-SDK-Version": "0.1.75",
-                "User-Agent": "@letta-ai/letta-client/0.1.75",
+                "X-Fern-SDK-Version": "0.1.76",
+                "User-Agent": "@letta-ai/letta-client/0.1.76",
                 "X-Fern-Runtime": core.RUNTIME.type,
                 "X-Fern-Runtime-Version": core.RUNTIME.version,
                 ...(await this._getCustomAuthorizationHeaders()),
